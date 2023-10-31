6d8561617d75a6e59f60111dff6218572672b1cc
NatashaWiener <149413843+NatashaWiener@users.noreply.github.com>
2023-10-31 20:44:47 +0100

    add Homework22.java

--- Homework22.java
+++ Homework22.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Homework22 {
+import java.util.Scanner;
+
+public class Homework22 {
+
+  public static void main(String[] args) {
+    System.out.println("hello word");
+    Scanner scanner = new Scanner(System.in);
+
+    int[] arr = new int[5];
+    for (int i = 0; i < 5; i = i + 1) {
+      arr[i] = scanner.nextInt();
+    }
+    for (int i = 0; i < 5; i = i + 1) {
+      if (arr[i] % 2 == 0) {
+        System.out.println(arr[i]);
+      }
+    }
+  }
+
+}
+
+
 }
